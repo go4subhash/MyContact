3899fa56bca963fdb4007dee5bc99df50dbecfd3
Subhash Chand <subhash@kiwitech.com>
2019-10-19 19:30:58 +0530

    Initial Commit

---
+++ MyContactApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyContactApp
+//
+//  Created by Subhash on 19/10/19.
+//  Copyright © 2019 Subhash. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
